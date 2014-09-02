44d6ea7bc308fbe6eada90fbde5dbce39749325f
Mike Regan <dreadknot@gmail.com>
2014-09-02 15:43:08 -0700

    add test app

---
+++ fooapp/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fooapp::Application.config.secret_key_base = 'e547dee33cee845571cb1ab23fa2644aac78573e099a650cc51d515014f54901df17c96ee9191efebbc04465cde11d9a1ff72f9af4fa02ed38d332d584337829'
